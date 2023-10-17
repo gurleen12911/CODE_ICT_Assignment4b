72d54b96119c52255d9f8cfb4998d2db0907d478
gurleen12911 <swt988@gmail.com>
2023-10-17 21:26:41 +0300

    display function

---
+++ display.py
@@ -0,0 +1,19 @@
+# display.py
+
+def display_list(my_list):
+    print("List contents:")
+    for element in my_list:
+        print(element)
+
+def display_menu():
+    print("Menu:")
+    print("1. Stop running the program")
+    print("2. Search for an element")
+    print("3. Add a new element")
+    print("4. Remove an element")
+    print("5. Sort the list (ascending)")
+    print("6. List all elements")
+    print("7. Count the total sum of elements")
+    print("8. Count the number of elements")
+    choice = input("Enter your choice: ")
+    return choice
